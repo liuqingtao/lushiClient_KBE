b9018106bd70b38b60d5079361dee854ca21c3f2
liuqingtao <lqtmail@163.com>
2018-06-27 16:25:23 +0800

    normal up

---
+++ Assets/scripts/battlefield/cardManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardManager : MonoBehaviour {
+
+    public GameObject cardPrefab;
+    public List<GameObject> cardList = new List<GameObject>();
+    public Transform[] areaPoint;
+    public Transform useCardPoint;
+    public float xOffset;
+    private void Start()
+    {
+        xOffset = (areaPoint[1].position.x - areaPoint[0].position.x)/9;
+    }
+    public void updateCardPostion()
+    {
+        
+        for(int i = 0; i < cardList.Count; i++)
+        {
+            cardList[i].transform.position = areaPoint[0].position + new Vector3(xOffset * i, 0, 0);
+            
+        }
+    }
+    public clientEntity initCard()
+    {
+        
+
+        GameObject obj = Instantiate(cardPrefab) as GameObject;
+        cardList.Add(obj);
+        obj.transform.SetParent(transform);
+        updateCardPostion();
+        return obj.GetComponent<singleCardController>();
+    }
+    public void removeCard(clientEntity scc)
+    {
+        GameObject obj0=null;
+        foreach(GameObject obj in cardList)
+        {
+            if(obj.GetComponent<singleCardController>() == scc)
+            {
+                obj0 = obj;
+            }
+        }
+        if(obj0 != null)
+        {
+            cardList.Remove(obj0);
+            Destroy(obj0);
+            updateCardPostion();
+        }
+        else
+        {
+            Debug.LogError("卡牌删除失败");
+        }
+        
+    }
+   
+              
+    
+}
---
+++ Assets/scripts/battlefield/followerManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followerManager : MonoBehaviour {
+
+    public float weight;
+    public float height;
+    public GameObject followerPrefab;
+    public List<GameObject> followerList;
+    private void Start()
+    {
+        weight = GetComponent<RectTransform>().rect.size.x/7;
+        height= GetComponent<RectTransform>().rect.size.y;
+
+    }
+    public clientEntity initFollower()
+    {
+        GameObject obj = Instantiate(followerPrefab) as GameObject;
+        followerList.Add(obj);
+        obj.transform.SetParent(transform);
+        obj.GetComponent<RectTransform>().sizeDelta = new Vector2(weight, height);
+        updateCardPostion();
+        return obj.GetComponent<singleFollowerController>();
+    }
+    public void updateCardPostion()
+    {
+        int sum = followerList.Count;
+        for (int i = 0; i < sum; i++)
+        {
+            followerList[i].transform.position = transform.position + new Vector3(-(followerList.Count / 2.0f - i - 0.5f) * weight, 0, 0);
+        }
+    }
+    public void removeFollower(clientEntity ce)
+    {
+        GameObject obj0 = null;
+        foreach (GameObject obj in followerList)
+        {
+            if (obj.GetComponent<singleFollowerController>() == ce)
+            {
+                obj0 = obj;
+            }
+        }
+        if (obj0 != null)
+        {
+            followerList.Remove(obj0);
+            Destroy(obj0);
+            updateCardPostion();
+        }
+        else
+        {
+            Debug.LogError("卡牌删除失败");
+        }
+
+    }
+}
