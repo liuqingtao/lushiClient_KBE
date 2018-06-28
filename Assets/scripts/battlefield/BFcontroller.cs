4527df87867b821d6d1308246f6c0723d5514d66
liuqingtao <lqtmail@163.com>
2018-06-28 15:10:26 +0800

    normal up

---
+++ Assets/scripts/battlefield/BFcontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KBEngine;
+
+public class BFcontroller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Account Me = KBEngineApp.app.player() as Account;
+        if(Me != null)
+        {
+            Me.baseCall("reqEnterBattlefield");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/kbe/Avatar.cs
@@ -0,0 +1,20 @@
+﻿namespace KBEngine
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using KBEngine;
+
+    public class Avatar : Entity
+    {
+        public override void __init__()
+        {
+            base.__init__();
+            Debug.Log("登陆成功 Avatar");
+        }
+
+
+    }
+
+}
+
